d2340e4c215dca97dd18d8b16ba8286af538ba3a
Davin <52686922+Frozies@users.noreply.github.com>
2022-09-09 13:24:35 -0500

    fix: updated phone regex
    https://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
    Typing in a phone number would break the regex because the string needed to start with either "00" or "\\" because of (\\+|00)

--- src/lib/modal/view.ts
+++ src/lib/modal/view.ts
@@ -52,7 +52,7 @@ export const createModal = async (props: {
     })
     .filter((p) => p !== undefined);
 
-  const phoneNumberRegex = `(\\+|00)[0-9]{1,3}[0-9]{4,14}(?:x.+)?$`;
+  const phoneNumberRegex = `^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$`;
   const emailRegex = `^([a-zA-Z0-9_.-])+(\+[a-zA-Z0-9-]+)?@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})`;
 
   // MODAL HTML
